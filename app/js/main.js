18c648d68a283cf71d90c502e02e4beed64e17f7
Martastartgit <marta.petrivska@gmail.com>
2020-05-23 23:30:52 +0300

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,15 @@ $(function(){
         readOnly: true
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        min: 0,
+        max: 10000,
+        from: 0,
+        to: 600,
+        type: 'double',
+        prefix: "$",
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 });
