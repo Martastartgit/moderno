d93c859a69f500daf04e3492805fd819dbb85705
Martastartgit <marta.petrivska@gmail.com>
2020-05-28 17:59:39 +0300

    start aside section

--- app/js/main.js
+++ app/js/main.js
@@ -50,6 +50,15 @@ $(function(){
         $('.menu__list').slideToggle();
 
     });
+    
+    $('.wrapper .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+        $('.wrapper .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
     
 
