18939dc53daa99321a9d1f3e37d638e5208e9420
Martastartgit <marta.petrivska@gmail.com>
2020-05-30 21:23:39 +0300

    added profile,settings and contact

--- app/js/main.js
+++ app/js/main.js
@@ -51,10 +51,10 @@ $(function(){
 
     });
     
-    $('.wrapper .tab').on('click', function (event) {
+    $('.wrapper .tab, .settings__tabs.tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
-        $('.wrapper .tabs').find('.tab').removeClass('active');
+        $('.wrapper, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.wrapper .tabs, .settings__tabs.tab').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
