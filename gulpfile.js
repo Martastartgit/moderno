4fb48f478535b92ba985f797bba8d47caad503e9
Martastartgit <marta.petrivska@gmail.com>
2020-06-01 12:56:02 +0300

    fixed plagin

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('style', function () {
         'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
-        'node_modules/jquery-form-styler/dist/jquery.formstyler.thema.css'
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
